2f2e3a9d13992199443fb4006f1848ea27bac287
SeungUkShin <krdut1@gmail.com>
2022-11-14 18:00:25 +0900

    Initial Commit

---
+++ Summar SwiftUI/Summar_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Summar_SwiftUIApp.swift
+//  Summar SwiftUI
+//
+//  Created by ukBook on 2022/11/14.
+//
+
+import SwiftUI
+
+@main
+struct Summar_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
